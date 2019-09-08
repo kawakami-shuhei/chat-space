0142bafa28fbd07e614338d3e4fed2276d58e622
kawakamishuhei <shuhei.k.1127.96@ezweb.ne.jp>
2019-09-08 13:19:33 +0900

    update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,55 @@
 $(function() {
+  function buildHTML(message) {
+    var body = message.body ? `${ message.body }` : "";
+    var img  = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message">
+                  <div class="message__upper-info">
+                    <div class="message__upper-info__talker">
+                      ${message.user_name}
+                    </div>
+                    <div class="message__upper-info__date">
+                      ${message.data}
+                    </div>
+                  </div>
+                  <div class="message__upper-text">
+                    <p class="lower-message__content">
+                      ${body}
+                    </p>
+                    ${img}                
+                  </div>
+                </div>`   
+    return html;
+  }
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    console.log(this)
-    var message = new FormDate(this);
-    var url = $(this).attr('create')
+    var formdata = new FormData(this);
+    var url = $(this).attr('create');
     $.ajax({
       url: url,
       type: "POST",
-      date: formdate,
-      datetype: 'json',
-      processDate: false,
+      data: formdata,
+      dataType: 'json',
+      processData: false,
       contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_body').val('');
+      $('#message_image').val('');
+    })
+    .fail(function(data) {
+      alert('エラーが発生したためメッセージは送信できませんでした');
+    })
+    .always(function(data) {
+      $('.submit-btn').prop('disabled', false);
+    })
+    .done(function scrollBottom(){
+      var target = $('.message').last();
+      var position = target.offset().top + $('.messages').scrollTop();
+      $('.messages').animate({
+        scrollTop: position
+      }, 300, 'swing');
     })
   })
 });
