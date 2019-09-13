cd962cc94e82651aea45ebe5a12a6dd888af1e6a
kawakamishuhei <shuhei.k.1127.96@ezweb.ne.jp>
2019-09-13 17:12:05 +0900

    change action in message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -22,7 +22,7 @@ $(document).on('turbolinks:load', function(){
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formdata = new FormData(this);
-    var url = $(this).attr('create');
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
